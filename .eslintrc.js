648d7a7136e1134d8240f2f2438e945baae46da9
okaryo <okaryotr@gmail.com>
2022-01-29 16:26:39 +0900

    [add] eslint and setting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'no-trailing-spaces': ['error']
+  },
+}
