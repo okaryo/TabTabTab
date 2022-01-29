d00e204b6bd9a0565ed326b7c34c71cae5bca5fe
okaryo <okaryotr@gmail.com>
2022-01-29 22:46:28 +0900

    [modify] package.json to remove css-loader and style-loader

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    index: path.join(__dirname, "src/popup/index.tsx"),
+    index: path.join(__dirname, "src/index.tsx"),
   },
   output: {
     path: path.join(__dirname, "dist/js"),
@@ -15,14 +15,6 @@ module.exports = {
         test: /\.tsx?/,
         use: "ts-loader"
       },
-      {
-        exclude: /node_modules/,
-        test: /\.css/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      }
     ]
   },
   resolve: {
