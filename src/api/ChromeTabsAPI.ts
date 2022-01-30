a8bb575c7386eb0bc3c13e49f17dfe3b0bd2fa04
okaryo <okaryotr@gmail.com>
2022-01-30 23:59:03 +0900

    [modify] ChromeTabsAPI to add getUnfocusedWindows method

--- src/api/ChromeTabsAPI.ts
+++ src/api/ChromeTabsAPI.ts
@@ -2,10 +2,9 @@ import { GroupedColor } from '../model/GroupedColor'
 import { GroupId } from '../model/GroupId'
 import { Tab } from '../model/Tab'
 import { TabId } from '../model/TabId'
-
 import { TbWindow } from '../model/Window'
 import { WindowId } from '../model/WindowId'
-
+import { TbWindows } from '../model/Windows'
 
 export class ChromeTabsAPI {
   static async getCurrentWindow(): Promise<TbWindow> {
@@ -33,5 +32,32 @@ export class ChromeTabsAPI {
       }
     }
     return currentWindow
+  }
+
+  static async getUnfocusedWindows(): Promise<TbWindows> {
+    const unfocusedWindowTabs = await chrome.tabs.query({ currentWindow: false })
+
+    let windows = TbWindows.empty()
+    for (const tab of unfocusedWindowTabs) {
+      const windowId = new WindowId(tab.windowId)
+      const newTab = new Tab(
+        new TabId(tab.id),
+        tab.title,
+        tab.favIconUrl,
+        tab.highlighted
+      )
+
+      if (tab.pinned) {
+        windows = windows.addPinnedTab(windowId, false, newTab)
+      } else if (tab.groupId !== chrome.tabGroups.TAB_GROUP_ID_NONE) {
+        const groupId = new GroupId(tab.groupId)
+        const group = await chrome.tabGroups.get(groupId.value)
+        const groupColor = new GroupedColor(group.color)
+        windows = windows.addGroupedTab(windowId, false, newTab, groupId, group.title, groupColor)
+      } else {
+        windows = windows.addTab(windowId, false, newTab)
+      }
+    }
+    return windows
   }
 }
