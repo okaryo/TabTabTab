d9d24de07d7880fb77f4ae5a64654504085180f2
okaryo <okaryotr@gmail.com>
2023-05-02 23:51:28 +0900

    :art: modify code by lint error

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import App from './view/App'
 import { createRoot } from 'react-dom/client'
 
 const app = document.getElementById('app')
-const root = createRoot(app!)
+const root = createRoot(app)
 root.render(<App />)
