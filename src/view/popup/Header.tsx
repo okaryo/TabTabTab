48df1ea9f9e066d3b7c9116a743d307d3fbd653e
okaryo <okaryotr@gmail.com>
2023-05-13 08:48:19 +0900

    :sparkles: add setting page link on popup appbar

--- src/view/popup/Header.tsx
+++ src/view/popup/Header.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import SettingsIcon from '@mui/icons-material/Settings'
 
-const Header = () => {
-  return (
-    <AppBar position="static" color="primary">
-      <Toolbar>
-        <Typography
-          style={{color:'white'}}
-          variant="h6"
-          component="h1"
-          sx={{ flexGrow: 1 }}
-        >
-          TabTabTab
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  )
-}
+const Header = () => (
+  <AppBar position="static" color="primary">
+    <Toolbar>
+      <Typography
+        style={{ color: 'white' }}
+        variant="h6"
+        component="h1"
+        sx={{ flexGrow: 1 }}
+      >
+        TabTabTab
+      </Typography>
+      <IconButton
+        onClick={() => window.open('chrome-extension://ieklamoilnjjpebpgahkefakcjfekeop/options.html')}
+        color="inherit"
+      >
+        <SettingsIcon />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
+)
 
 export default Header
