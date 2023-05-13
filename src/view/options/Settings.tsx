c5bdc0a08f01fcdd7fd8c5c24b17bb338332d3ae
okaryo <okaryotr@gmail.com>
2023-05-13 09:37:36 +0900

    :sparkles: add review encouragement text to setting page

--- src/view/options/Settings.tsx
+++ src/view/options/Settings.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import AutoTabCloseSettingForm from './AutoTabCloseSettingForm'
+import { Divider, Link, Typography } from '@mui/material'
 
 const Settings = () => {
   return (
-    <AutoTabCloseSettingForm />
+    <>
+      <AutoTabCloseSettingForm />
+
+      <Divider sx={{ mt: 2, mb: 2 }} />
+
+      <Typography
+        variant="caption"
+        component="p"
+      >
+        If you find TabTabTab helpful, we would greatly appreciate it if you could leave us a review on the&nbsp;
+        <Link
+          href="https://chrome.google.com/webstore/detail/tabtabtab/hfmnidllojimehmfjkclnadpebibhgoi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Chrome Store
+        </Link>
+        ! Your voice greatly contributes to the development of TabTabTab.
+      </Typography>
+    </>
   )
 }
 
