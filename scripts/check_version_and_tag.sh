0c7b780d8ffafb7c7634e637b0039a9798e9684c
okaryo <okaryotr@gmail.com>
2023-05-30 13:05:41 +0900

    :sparkles: add script to check version and tag

---
+++ scripts/check_version_and_tag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: ./scripts/check_version_and_tag.sh 1.0.0
+# If the script does not have execute permissions, run `chmod +x ./scripts/check_version_and_tag.sh` to grant it.`
+
+# Get argument
+tag_version=$1
+
+# Extract version information from package.json and manifest.json
+package_version=$(node -p "require('./package.json').version")
+manifest_version=$(node -p "require('./dist/manifest.json').version")
+
+# Confirm that the versions match
+if [ "$tag_version" != "$package_version" ] || [ "$tag_version" != "$manifest_version" ]; then
+  echo "Error: version mismatch. Ensure that the version in package.json and dist/manifest.json matches the tag version."
+  exit 1
+fi
+
+# Create a tag
+git tag $tag_version
