8f3c57fd657fd7c99ad2e340151a44e7f15816cc
okaryo <okaryotr@gmail.com>
2023-06-04 12:27:30 +0900

    :sparkles: add eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
   ],
   parserOptions: {
     project: "./tsconfig.json",
