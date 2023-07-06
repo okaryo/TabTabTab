be2eed82af0e7ff47da597b2665395224662819d
okaryo <okaryotr@gmail.com>
2023-07-07 00:06:25 +0900

    :sparkles: add quick search feature

--- src/view/features/popup/pages/Home.tsx
+++ src/view/features/popup/pages/Home.tsx
@@ -1,21 +1,41 @@
 import Box from "@mui/material/Box";
-import React, { useState } from "react";
+import List from "@mui/material/List";
+import React, { useContext, useState } from "react";
 
 import Header from "../components/Header";
+import TabItem from "../components/TabItem";
 import TabList from "../components/TabList";
 import WindowTabs from "../components/WindowTabs";
+import { WindowsContext } from "../contexts/Windows";
 
 const Home = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [searchText, setSearchText] = React.useState("");
+  const { windows } = useContext(WindowsContext);
+  const tabs = windows.findTabsByTitleOrUrl(searchText);
 
   return (
     <Box>
-      <Header />
-      <WindowTabs
-        selectedIndex={selectedIndex}
-        onSelectIndex={setSelectedIndex}
-      />
-      <TabList selectedWindowIndex={selectedIndex} />
+      <Header onChange={setSearchText} />
+      {searchText.length > 0 && (
+        <List
+          sx={{ width: "100%", bgcolor: "background.paper" }}
+          disablePadding
+        >
+          {tabs.map((tab) => (
+            <TabItem key={tab.id.value} tab={tab} />
+          ))}
+        </List>
+      )}
+      {searchText.length === 0 && (
+        <>
+          <WindowTabs
+            selectedIndex={selectedIndex}
+            onSelectIndex={setSelectedIndex}
+          />
+          <TabList selectedWindowIndex={selectedIndex} />
+        </>
+      )}
     </Box>
   );
 };
