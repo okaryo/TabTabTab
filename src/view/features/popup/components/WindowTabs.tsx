e6a6e9ae98bb916450056cd29782ae4dff0120cf
okaryo <okaryotr@gmail.com>
2023-07-16 09:24:15 +0900

    :bug: show no search result text

--- src/view/features/popup/components/WindowTabs.tsx
+++ src/view/features/popup/components/WindowTabs.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
-import React, { useContext } from "react";
+import React, { Dispatch, useContext, useEffect } from "react";
 
 import t from "../../../../i18n/Translations";
 import { WindowsContext } from "../contexts/Windows";
@@ -9,12 +9,34 @@ import WindowTab from "./WindowTab";
 
 type WindowTabsProps = {
   selectedIndex: number;
-  onSelectIndex: (index: number) => void;
+  onSelectIndex: Dispatch<React.SetStateAction<number>>;
 };
 
 const WindowTabs = (props: WindowTabsProps) => {
   const { selectedIndex, onSelectIndex } = props;
   const { windows } = useContext(WindowsContext);
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        const maxWindowIndex = windows.length - 1;
+        onSelectIndex((oldIndex) =>
+          oldIndex === maxWindowIndex ? 0 : oldIndex + 1,
+        );
+      } else if (event.key === "ArrowLeft") {
+        const minWindowIndex = 0;
+        onSelectIndex((oldIndex) =>
+          oldIndex === minWindowIndex ? windows.length - 1 : oldIndex - 1,
+        );
+      }
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onSelectIndex, windows.length]);
 
   const onChange = (_: React.SyntheticEvent, newValue: number) => {
     onSelectIndex(newValue);
