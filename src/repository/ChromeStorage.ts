88aaffd0a9f29d5b981e1c859cd23c76c1bb78c9
okaryo <okaryotr@gmail.com>
2023-12-23 11:29:54 +0900

    feat: save recent active tabs

--- src/repository/ChromeStorage.ts
+++ src/repository/ChromeStorage.ts
@@ -1,6 +1,6 @@
 import { DurationUnit } from "../model/settings/TabCleaner";
 
-export type TabCleanerSettingStoredData = {
+export type TabCleanerSettingStorageObject = {
   tab_cleaner_setting: {
     isEnabled: boolean;
     duration: number;
@@ -8,19 +8,19 @@ export type TabCleanerSettingStoredData = {
   };
 };
 
-export type PopupSizeSettingStoredData = {
+export type PopupSizeSettingStorageObject = {
   popup_size_setting: {
     height: number;
     width: number;
   };
 };
 
-export type PopupElementScaleSettingStoredData = {
+export type PopupElementScaleSettingStorageObject = {
   popup_element_scale: number;
 };
 
 type Theme = "light" | "dark";
-export type ThemeStoredData = {
+export type ThemeStorageObject = {
   theme: Theme;
 };
 
@@ -28,8 +28,24 @@ type DateString = string;
 type StoredLastActivatedAt = {
   [tabId: string]: DateString;
 };
-export type LastActivatedAtStoredData = {
+export type LastActivatedAtStorageObject = {
   last_activated_at: StoredLastActivatedAt;
+};
+
+export type SerializedTab = {
+  id: number;
+  groupId: number | null;
+  windowId: number | null;
+  title: string;
+  url: string;
+  favIconUrl: string | null;
+  highlighted: boolean;
+  audible: boolean;
+  pinned: boolean;
+  lastActivatedAt: DateString | null;
+};
+export type RecentActiveTabsStorageObject = {
+  recent_active_tabs: SerializedTab[];
 };
 
 export class ChromeLocalStorage {
@@ -41,4 +57,5 @@ export class ChromeLocalStorage {
 
 export class ChromeSessionStorage {
   static readonly LAST_ACTIVATED_AT_KEY = "last_activated_at";
+  static readonly RECENT_ACTIVE_TABS_KEY = "recent_active_tabs";
 }
--- src/repository/TabsRepository.ts
+++ src/repository/TabsRepository.ts
@@ -1,18 +1,26 @@
+/* eslint @typescript-eslint/no-floating-promises: 0 */
+
 import { Tab, TabId } from "../model/Tab";
 import { WindowId } from "../model/Window";
 
 import {
   ChromeSessionStorage,
-  LastActivatedAtStoredData,
+  LastActivatedAtStorageObject,
+  RecentActiveTabsStorageObject,
+  SerializedTab,
 } from "./ChromeStorage";
 
-export const focusTab = async (tabId: number) => {
-  const tab = await chrome.tabs.get(tabId);
-  const windowId = tab.windowId;
-
-  /* eslint @typescript-eslint/no-floating-promises: 0 */
-  chrome.windows.update(windowId, { focused: true });
-  chrome.tabs.update(tabId, { active: true });
+export const focusTab = async (tab: Tab) => {
+  const isExistingTab = await chrome.tabs
+    .get(tab.id)
+    .then(() => true)
+    .catch(() => false);
+  if (isExistingTab) {
+    chrome.windows.update(tab.windowId, { focused: true });
+    chrome.tabs.update(tab.id, { active: true });
+  } else {
+    chrome.tabs.create({ url: tab.url.toString() });
+  }
 };
 
 export const removeTab = async (tabId: number) => {
@@ -22,7 +30,7 @@ export const removeTab = async (tabId: number) => {
 export const updateLastActivatedAtOfTab = async (tabId: number) => {
   let { last_activated_at } = (await chrome.storage.session.get(
     ChromeSessionStorage.LAST_ACTIVATED_AT_KEY,
-  )) as LastActivatedAtStoredData;
+  )) as LastActivatedAtStorageObject;
   if (last_activated_at === undefined) last_activated_at = {};
 
   last_activated_at[tabId] = new Date().toISOString();
@@ -34,7 +42,7 @@ export const updateLastActivatedAtOfTab = async (tabId: number) => {
 export const deleteLastActivatedAtOfTab = async (tabId: number) => {
   const { last_activated_at } = (await chrome.storage.session.get(
     ChromeSessionStorage.LAST_ACTIVATED_AT_KEY,
-  )) as LastActivatedAtStoredData;
+  )) as LastActivatedAtStorageObject;
   delete last_activated_at[tabId];
   await chrome.storage.session.set({
     [ChromeSessionStorage.LAST_ACTIVATED_AT_KEY]: last_activated_at,
@@ -95,4 +103,99 @@ export const unpinAllTabs = async (tabs: Tab[]) => {
 export const closeAllTabs = async (tabs: Tab[]) => {
   const ids = tabs.map((tab) => tab.id);
   await chrome.tabs.remove(ids);
+};
+
+export const getRecentActiveTabs = async (): Promise<Tab[]> => {
+  let { recent_active_tabs: recentActiveTabs } =
+    (await chrome.storage.session.get(
+      ChromeSessionStorage.RECENT_ACTIVE_TABS_KEY,
+    )) as RecentActiveTabsStorageObject;
+  if (recentActiveTabs === undefined) recentActiveTabs = [];
+
+  return recentActiveTabs.map((serializedTab) =>
+    deserializeToTab(serializedTab),
+  );
+};
+
+export const updateRecentActiveTabs = async (tabId: number) => {
+  const tab = await getTabBy(tabId);
+  const serializedTab = serializeTab(tab);
+
+  let { recent_active_tabs: recentActiveTabs } =
+    (await chrome.storage.session.get(
+      ChromeSessionStorage.RECENT_ACTIVE_TABS_KEY,
+    )) as RecentActiveTabsStorageObject;
+  if (recentActiveTabs === undefined) recentActiveTabs = [];
+
+  const uniqueTabs = recentActiveTabs.filter((recentActiveTab) => {
+    const isDuplicated =
+      recentActiveTab.id === tab.id ||
+      (recentActiveTab.title === tab.title &&
+        recentActiveTab.url === tab.url.toString());
+    return !isDuplicated;
+  });
+
+  const newTabs = [serializedTab, ...uniqueTabs].slice(0, 10);
+  await chrome.storage.session.set({
+    [ChromeSessionStorage.RECENT_ACTIVE_TABS_KEY]: newTabs,
+  });
+};
+
+const getTabBy = async (tabId: number): Promise<Tab> => {
+  const tab = await chrome.tabs.get(tabId);
+  const { last_activated_at } = (await chrome.storage.session.get(
+    ChromeSessionStorage.LAST_ACTIVATED_AT_KEY,
+  )) as LastActivatedAtStorageObject;
+
+  return {
+    id: tab.id,
+    groupId:
+      tab.groupId === chrome.tabGroups.TAB_GROUP_ID_NONE ? null : tab.groupId,
+    windowId:
+      tab.windowId === chrome.windows.WINDOW_ID_NONE ? null : tab.windowId,
+    title: tab.title,
+    url: tab.url && tab.url !== "" ? new URL(tab.url) : new URL(tab.pendingUrl),
+    favIconUrl:
+      tab.favIconUrl && tab.favIconUrl !== "" ? new URL(tab.favIconUrl) : null,
+    highlighted: tab.highlighted,
+    audible: tab.audible,
+    pinned: tab.pinned,
+    lastActivatedAt: last_activated_at[tabId]
+      ? new Date(last_activated_at[tabId])
+      : null,
+  };
+};
+
+const serializeTab = (tab: Tab) => {
+  return {
+    id: tab.id,
+    groupId: tab.groupId,
+    windowId: tab.windowId,
+    title: tab.title,
+    url: tab.url.toString(),
+    favIconUrl: tab.favIconUrl?.toString(),
+    highlighted: false,
+    audible: false,
+    pinned: false,
+    lastActivatedAt: tab.lastActivatedAt?.toISOString(),
+  };
+};
+
+const deserializeToTab = (serializedTab: SerializedTab): Tab => {
+  return {
+    id: serializedTab.id,
+    groupId: serializedTab.groupId,
+    windowId: serializedTab.windowId,
+    title: serializedTab.title,
+    url: new URL(serializedTab.url),
+    favIconUrl: serializedTab.favIconUrl
+      ? new URL(serializedTab.favIconUrl)
+      : null,
+    highlighted: serializedTab.highlighted,
+    audible: serializedTab.audible,
+    pinned: serializedTab.pinned,
+    lastActivatedAt: serializedTab.lastActivatedAt
+      ? new Date(serializedTab.lastActivatedAt)
+      : null,
+  };
 };
