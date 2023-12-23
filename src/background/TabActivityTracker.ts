aeddbc4b0d78540620a917ba323123591af9b1c2
okaryo <okaryotr@gmail.com>
2023-12-23 11:30:17 +0900

    feat: update recent active tabs on tab events

--- src/background/TabActivityTracker.ts
+++ src/background/TabActivityTracker.ts
@@ -2,27 +2,33 @@
 import {
   deleteLastActivatedAtOfTab,
   updateLastActivatedAtOfTab,
+  updateRecentActiveTabs,
 } from "../repository/TabsRepository";
 
 export const addListenerOnTabActivated = () => {
   chrome.tabs.onActivated.addListener((activeInfo) => {
     const { tabId } = activeInfo;
     updateLastActivatedAtOfTab(tabId);
+    updateRecentActiveTabs(tabId);
   });
   chrome.tabs.onAttached.addListener((tabId) => {
     updateLastActivatedAtOfTab(tabId);
+    updateRecentActiveTabs(tabId);
   });
   chrome.tabs.onHighlighted.addListener((highlightInfo) => {
     const { tabIds } = highlightInfo;
     for (const tabId of tabIds) {
       updateLastActivatedAtOfTab(tabId);
+      updateRecentActiveTabs(tabId);
     }
   });
   chrome.tabs.onMoved.addListener((tabId) => {
     updateLastActivatedAtOfTab(tabId);
+    updateRecentActiveTabs(tabId);
   });
   chrome.tabs.onUpdated.addListener((tabId) => {
     updateLastActivatedAtOfTab(tabId);
+    updateRecentActiveTabs(tabId);
   });
 };
 
