03b90c8af4321507175a247ebf3b156d0c9215c9
okaryo <okaryotr@gmail.com>
2023-12-27 10:27:44 +0900

    feat: add WindowsProvider

---
+++ src/view/features/options/WindowsProvider.tsx
@@ -0,0 +1,12 @@
+import { WindowsContext } from "../../contexts/Windows";
+import { useWindows } from "../../hooks/useWindows";
+
+const WindowsProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WindowsContext.Provider value={useWindows()}>
+      {children}
+    </WindowsContext.Provider>
+  );
+};
+
+export default WindowsProvider;
