6ddc9de911d689823965854098b13ddb19f3268d
okaryo <okaryotr@gmail.com>
2024-02-10 18:22:09 +0900

    fix: unstable close window action

--- src/view/features/options/hooks/useCloseWindow.ts
+++ src/view/features/options/hooks/useCloseWindow.ts
@@ -1,18 +1,15 @@
 import { useCallback, useContext } from "react";
 
-import {
-  closeWindow,
-  getWindows,
-} from "../../../../repository/WindowsRepository";
+import { Window } from "../../../../model/Window";
+import { closeWindow } from "../../../../repository/WindowsRepository";
 import { WindowsContext } from "../../../contexts/WindowsContext";
 
-export const useCloseWindow = (): ((id: number) => Promise<void>) => {
+export const useCloseWindow = (): ((window: Window) => Promise<void>) => {
   const { setWindows } = useContext(WindowsContext);
 
   const callback = useCallback(
-    async (id: number) => {
-      await closeWindow(id);
-      const newWindows = await getWindows();
+    async (window: Window) => {
+      const newWindows = await closeWindow(window);
       setWindows(newWindows);
     },
     [setWindows],
