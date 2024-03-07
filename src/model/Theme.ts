46941b2b563935ecfefe2e752595f9217f969fd5
okaryo <okaryotr@gmail.com>
2024-03-08 00:48:25 +0900

    feat: add ThemeColor type

---
+++ src/model/Theme.ts
@@ -0,0 +1,27 @@
+export type Mode = "light" | "dark";
+export type ThemeColor =
+  | "grey"
+  | "blue"
+  | "red"
+  | "yellow"
+  | "green"
+  | "pink"
+  | "purple"
+  | "cyan"
+  | "orange";
+
+export const defaultMode: Mode = "light";
+
+export const defaultThemeColor = "blue";
+
+export const themeColors: ThemeColor[] = [
+  "grey",
+  "blue",
+  "red",
+  "yellow",
+  "green",
+  "pink",
+  "purple",
+  "cyan",
+  "orange",
+];
