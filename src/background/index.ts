19cb5bee977a5effb42f16fbe8c9be6a25aabc4e
okaryo <okaryotr@gmail.com>
2024-08-12 17:46:01 +0900

    chore: show DEV badge text when development mode

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,6 +2,10 @@ import { addTabGroupingListeners } from "./AutoTabGrouping";
 import { addTabAccessesListener } from "./TabActivityTracker";
 import { activateTabCleanerScheduler } from "./TabCleanerScheduler";
 
+if (import.meta.env.DEV) {
+  chrome.action.setBadgeText({ text: 'DEV' });
+}
+
 // TabActivityTracker
 addTabAccessesListener();
 
