17ee9b7e82aeb274fd8b31e02e2fbbfdb83e959e
okaryo <okaryotr@gmail.com>
2025-01-16 14:51:15 +0900

    refine: add footer to explain key navigation

--- src/presentation/views/options/components/TabSearchForm.tsx
+++ src/presentation/views/options/components/TabSearchForm.tsx
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
 import Container from "@mui/material/Container";
 import Dialog from "@mui/material/Dialog";
+import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import List from "@mui/material/List";
@@ -133,6 +134,137 @@ const SearchedTabs = (props: SearchedTabsProps) => {
   );
 };
 
+const DialogFooter = () => {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      component="footer"
+      sx={{ py: 1, px: 1.5 }}
+      direction="row"
+      spacing={2}
+    >
+      <Typography
+        variant="caption"
+        color="textSecondary"
+        sx={{ display: "flex", alignItems: "center", gap: 1 }}
+      >
+        <Box
+          component="kbd"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: theme.palette.action.selected,
+            borderRadius: 4,
+            width: "20px",
+            height: "18px",
+          }}
+        >
+          <svg width="15" height="15" aria-label="Enter key" role="img">
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.2"
+            >
+              <path d="M12 3.53088v3c0 1-1 2-2 2H4M7 11.53088l-3-3 3-3" />
+            </g>
+          </svg>
+        </Box>
+        <span>to select</span>
+      </Typography>
+      <Typography
+        variant="caption"
+        color="textSecondary"
+        sx={{ display: "flex", alignItems: "center", gap: 1 }}
+      >
+        <Box
+          component="kbd"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: theme.palette.action.selected,
+            borderRadius: 4,
+            width: "20px",
+            height: "18px",
+          }}
+        >
+          <svg width="15" height="15" aria-label="Arrow down" role="img">
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.2"
+            >
+              <path d="M7.5 3.5v8M10.5 8.5l-3 3-3-3" />
+            </g>
+          </svg>
+        </Box>
+        <Box
+          component="kbd"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: theme.palette.action.selected,
+            borderRadius: 4,
+            width: "20px",
+            height: "18px",
+          }}
+        >
+          <svg width="15" height="15" aria-label="Arrow up" role="img">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.2"
+            >
+              <path d="M7.5 11.5v-8M10.5 6.5l-3-3-3 3" />
+            </g>
+          </svg>
+        </Box>
+        to navigate
+      </Typography>
+      <Typography
+        variant="caption"
+        color="textSecondary"
+        sx={{ display: "flex", alignItems: "center", gap: 1 }}
+      >
+        <Box
+          component="kbd"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: theme.palette.action.selected,
+            borderRadius: 4,
+            width: "20px",
+            height: "18px",
+          }}
+        >
+          <svg width="15" height="15" aria-label="Escape key" role="img">
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.2"
+            >
+              <path d="M13.6167 8.936c-.1065.3583-.6883.962-1.4875.962-.7993 0-1.653-.9165-1.653-2.1258v-.5678c0-1.2548.7896-2.1016 1.653-2.1016.8634 0 1.3601.4778 1.4875 1.0724M9 6c-.1352-.4735-.7506-.9219-1.46-.8972-.7092.0246-1.344.57-1.344 1.2166s.4198.8812 1.3445.9805C8.465 7.3992 8.968 7.9337 9 8.5c.032.5663-.454 1.398-1.4595 1.398C6.6593 9.898 6 9 5.963 8.4851m-1.4748.5368c-.2635.5941-.8099.876-1.5443.876s-1.7073-.6248-1.7073-2.204v-.4603c0-1.0416.721-2.131 1.7073-2.131.9864 0 1.6425 1.031 1.5443 2.2492h-2.956" />
+            </g>
+          </svg>
+        </Box>
+        to close
+      </Typography>
+    </Stack>
+  );
+};
+
 type SearchDialogProps = {
   open: boolean;
   onClose: () => void;
@@ -185,6 +317,7 @@ const SearchDialog = (props: SearchDialogProps) => {
 
       if (["ArrowUp", "ArrowDown"].includes(event.key)) {
         event.preventDefault();
+
         if (event.key === "ArrowDown") {
           setSelectedItemIndex((oldIndex) =>
             oldIndex === maxIndex ? minIndex : oldIndex + 1,
@@ -270,6 +403,8 @@ const SearchDialog = (props: SearchDialogProps) => {
           />
         )}
       </Box>
+      <Divider />
+      <DialogFooter />
     </Dialog>
   );
 };
