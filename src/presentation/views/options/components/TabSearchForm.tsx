38f822baf9b1a190224d280f095b942479f87024
okaryo <okaryotr@gmail.com>
2025-01-16 15:19:38 +0900

    fix: svg attribute name

--- src/presentation/views/options/components/TabSearchForm.tsx
+++ src/presentation/views/options/components/TabSearchForm.tsx
@@ -220,9 +220,9 @@ const DialogFooter = () => {
             <g
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.2"
             >
               <path d="M7.5 11.5v-8M10.5 6.5l-3-3-3 3" />
             </g>
